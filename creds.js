3e0d96d906ca1452d7cb2349622bf53a95fb7853
Dean Petersen <61034935+dwpetersen@users.noreply.github.com>
2022-08-06 14:26:26 +1000

    Removed console.log for saving alias

--- creds.js
+++ creds.js
@@ -2,15 +2,14 @@ const fs = require('fs');
 
 const CREDS_PATH = 'creds';
 
-const openAlias = (aliasName) => {
-    const aliasFile = fs.readFileSync(`${CREDS_PATH}/${aliasName}.json`);
+const openAlias = (name) => {
+    const aliasFile = fs.readFileSync(`${CREDS_PATH}/${name}.json`);
     const alias = JSON.parse(aliasFile);
     alias.lastRequest = alias.lastRequest ? new Date(alias.lastRequest) : undefined;
     return alias;
 } 
 
 const saveAlias = (alias) => {
-    console.log(alias);
     fs.writeFileSync(`${CREDS_PATH}/${alias.name}.json`, JSON.stringify(alias, null, 4));
 };
 
