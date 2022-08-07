806c05aed0c2a47c06a515a2001bc1ba92f19137
Dean Petersen <61034935+dwpetersen@users.noreply.github.com>
2022-08-07 12:47:50 +1000

    Created test for SOQLQuery.from()

--- src/__tests__/query.test.ts
+++ src/__tests__/query.test.ts
@@ -1,8 +1,15 @@
 import { SOQLQuery } from '../query'
 
-test('select adds items to selectItems', () => {
+test('select() adds items to selectItems', () => {
     const query = new SOQLQuery();
     query.select('Id', 'Name');
     expect(query.selectItems.includes('Id'));
     expect(query.selectItems.includes('Name'));
+});
+
+test('from() sets fromValue to sObject', () => {
+    const sObject = 'Account';
+    const query = new SOQLQuery();
+    query.from(sObject)
+    expect(query.fromValue).toBe(sObject);
 });
