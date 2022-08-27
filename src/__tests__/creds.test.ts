5f492cea15f80800cd04a7c0191090694aed1b9e
Dean Petersen <61034935+dwpetersen@users.noreply.github.com>
2022-08-27 17:42:19 +1000

    Created tests for openAlias()

---
+++ src/__tests__/creds.test.ts
@@ -0,0 +1,35 @@
+import * as creds from '../creds';
+import * as fs from 'fs';
+import * as factory from './utils/factory'
+
+test('openAlias() loads alias', () => {
+    //Given
+    const alias = factory.createAlias();
+    const aliasJSON = JSON.stringify(alias) 
+    const mockReadFileSync = jest.spyOn(fs, 'readFileSync');
+    mockReadFileSync.mockReturnValue(Buffer.from(aliasJSON));
+
+    //When
+    const actualAlias = creds.openAlias(alias.name);
+
+    //Then
+    expect(actualAlias).toEqual(alias);
+});
+
+test('when lastRequest is set, openAlias() converts ISO string to Date object', () => {
+    //Given
+    const alias = factory.createAlias(new Date());
+    const aliasJSON = JSON.stringify(alias) 
+    const mockReadFileSync = jest.spyOn(fs, 'readFileSync');
+    mockReadFileSync.mockReturnValue(Buffer.from(aliasJSON));
+
+    //When
+    const actualAlias = creds.openAlias(alias.name);
+
+    //Then
+    expect(actualAlias).toEqual(alias);
+});
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
