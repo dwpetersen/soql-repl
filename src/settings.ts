7f9118e23e23ed141a72e0ff3d8285cfad77e918
Dean Petersen <61034935+dwpetersen@users.noreply.github.com>
2022-08-28 20:30:30 +1000

    Fixed lint issues with settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,10 +1,14 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-const openSettings = () => {
+interface Settings {
+    defaultAlias: string
+}
+
+function openSettings(): Settings {
     const settingsPath = path.resolve('config', 'settings.json');
     const settingsFile = fs.readFileSync(settingsPath);
-    return JSON.parse(settingsFile.toString());
+    return JSON.parse(settingsFile.toString()) as Settings;
 }
 
 export const settings = openSettings();
