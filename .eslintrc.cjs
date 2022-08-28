1f321f4f52d563e52b956fca9051562d767c4abf
Dean Petersen <61034935+dwpetersen@users.noreply.github.com>
2022-08-28 17:24:35 +1000

    Added ts configuration for ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,16 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+    },
     plugins: ['@typescript-eslint'],
     root: true,
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    include: [
+        'src/**/*'
+    ]
 };
