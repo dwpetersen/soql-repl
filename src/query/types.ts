b51c7315b3474e6ac5b265f4577c1930f3e5b4e4
Dean Petersen <61034935+dwpetersen@users.noreply.github.com>
2022-08-30 23:04:01 +1000

    Created function isDateLiteral

--- src/query/types.ts
+++ src/query/types.ts
@@ -8,10 +8,21 @@ export type DateLiteral = 'YESTERDAY'|'TODAY'|'TOMORROW'|
                           'LAST_90_DAYS'|'NEXT_90_DAYS'|'THIS_QUARTER'|
                           'LAST_QUARTER'|'NEXT_QUARTER'|'THIS_YEAR'|
                           'NEXT_YEAR'|'THIS_FISCAL_QUARTER'|'LAST_FISCAL_QUARTER'|
-                          'NEXT_FISCAL_QUARTER'|'THIS_FISCAL_YEAR'|'NEXT_FISCAL_YEAR'
+                          'NEXT_FISCAL_QUARTER'|'THIS_FISCAL_YEAR'|'NEXT_FISCAL_YEAR';
+
+export function isDateLiteral(value: string): value is DateLiteral {
+    const options = new Set(['YESTERDAY','TODAY','TOMORROW',
+                             'LAST_WEEK','THIS_WEEK','NEXT_WEEK',
+                             'LAST_MONTH','THIS_MONTH','NEXT_MONTH',
+                             'LAST_90_DAYS','NEXT_90_DAYS','THIS_QUARTER',
+                             'LAST_QUARTER','NEXT_QUARTER','THIS_YEAR',
+                             'NEXT_YEAR','THIS_FISCAL_QUARTER','LAST_FISCAL_QUARTER',
+                             'NEXT_FISCAL_QUARTER','THIS_FISCAL_YEAR','NEXT_FISCAL_YEAR']);
+    return options.has(value);
+}
 
 export type LogicalOperator = 'AND'|'OR'|'NOT';
 
 export type Operator = ComparisonOperator|LogicalOperator
 
-export type Operand = string|number|Date|null;
+export type Operand = string|number|Date|null|DateLiteral;
