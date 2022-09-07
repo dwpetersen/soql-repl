8126a691986295aa50c33ae5f115bed4b8c4ad38
Dean Petersen <61034935+dwpetersen@users.noreply.github.com>
2022-09-07 22:30:00 +1000

    Updated tests in creds

--- src/creds.test.ts
+++ src/creds.test.ts
@@ -4,47 +4,163 @@ import * as fs from 'fs';
 import * as factory from './__tests__/utils/factory'
 import * as path from 'path';
 
-test('openAlias() loads PasswordAlias', () => {
-    //Given
-    const alias: PasswordAlias = factory.createPasswordAlias();
-    const aliasJSON = JSON.stringify(alias);
-    const mockReadFileSync = jest.spyOn(fs, 'readFileSync');
-    mockReadFileSync.mockReturnValue(Buffer.from(aliasJSON));
+describe('openAlias()', () => {
+    test('loads PasswordAlias', () => {
+        //Given
+        const alias: PasswordAlias = factory.createPasswordAlias();
+        const aliasJSON = JSON.stringify(alias);
+        const mockReadFileSync = jest.spyOn(fs, 'readFileSync');
+        mockReadFileSync.mockReturnValue(Buffer.from(aliasJSON));
 
-    //When
-    const actualAlias = creds.openAlias(alias.name);
+        //When
+        const actualAlias = creds.openAlias(alias.name);
 
-    //Then
-    expect(actualAlias).toEqual(alias);
+        //Then
+        expect(actualAlias).toEqual(alias);
+    });
+
+    test('loads OAuthAlias', () => {
+        //Given
+        const alias: OAuthAlias = factory.createOAuthAlias();
+        const aliasJSON = JSON.stringify(alias);
+        const mockReadFileSync = jest.spyOn(fs, 'readFileSync');
+        mockReadFileSync.mockReturnValue(Buffer.from(aliasJSON));
+
+        //When
+        const actualAlias = creds.openAlias(alias.name);
+
+        //Then
+        expect(actualAlias).toEqual(alias);
+    });
+
+    test('when lastRequest is set, converts ISO string to Date object', () => {
+        //Given
+        const alias = factory.createPasswordAlias(new Date());
+        const aliasJSON = JSON.stringify(alias);
+        const mockReadFileSync = jest.spyOn(fs, 'readFileSync');
+        mockReadFileSync.mockReturnValue(Buffer.from(aliasJSON));
+
+        //When
+        const actualAlias = creds.openAlias(alias.name);
+
+        //Then
+        expect(actualAlias.lastRequest).toBeInstanceOf(Date);
+        expect(actualAlias).toEqual(alias);
+    });
 });
 
-test('openAlias() loads OAuthAlias', () => {
-    //Given
-    const alias: OAuthAlias = factory.createOAuthAlias();
-    const aliasJSON = JSON.stringify(alias);
-    const mockReadFileSync = jest.spyOn(fs, 'readFileSync');
-    mockReadFileSync.mockReturnValue(Buffer.from(aliasJSON));
+describe('isAlias()', () => {
+    test('OAuthAlias is an Alias', () => {
+        //Given
+        const alias = factory.createOAuthAlias(new Date('2000-01-01'));
 
-    //When
-    const actualAlias = creds.openAlias(alias.name);
+        //When
+        const isAlias = creds.isAlias(alias);
 
-    //Then
-    expect(actualAlias).toEqual(alias);
+        //Then
+        expect(isAlias).toBeTruthy();
+    });
+
+    test('PasswordAlias is an Alias', () => {
+        //Given
+        const alias = factory.createPasswordAlias(new Date('2000-01-01'));
+
+        //When
+        const isAlias = creds.isAlias(alias);
+
+        //Then
+        expect(isAlias).toBeTruthy();
+    });
+
+    test('Different object is not an Alias', () => {
+        //Given
+        const alias = {
+            things: 'stuff'
+        }
+
+        //When
+        const isAlias = creds.isAlias(alias);
+
+        //Then
+        expect(isAlias).toBeFalsy();
+    });
 });
 
-test('when lastRequest is set, openAlias() converts ISO string to Date object', () => {
-    //Given
-    const alias = factory.createPasswordAlias(new Date());
-    const aliasJSON = JSON.stringify(alias);
-    const mockReadFileSync = jest.spyOn(fs, 'readFileSync');
-    mockReadFileSync.mockReturnValue(Buffer.from(aliasJSON));
+describe('isOAuthAlias()', () => {
+    test('OAuthAlias is an OAuthAlias', () => {
+        //Given
+        const alias = factory.createOAuthAlias(new Date('2000-01-01'));
 
-    //When
-    const actualAlias = creds.openAlias(alias.name);
+        //When
+        const isOAuth = creds.isOAuthAlias(alias);
 
-    //Then
-    expect(actualAlias.lastRequest).toBeInstanceOf(Date);
-    expect(actualAlias).toEqual(alias);
+        //Then
+        expect(isOAuth).toBeTruthy();
+    });
+
+    test('PasswordAlias is not an OAuthAlias', () => {
+        //Given
+        const alias = factory.createPasswordAlias(new Date('2000-01-01'));
+
+        //When
+        const isOAuth = creds.isOAuthAlias(alias);
+
+        //Then
+        expect(isOAuth).toBeFalsy();
+    });
+
+
+    test('Different object is not an OAuthAlias', () => {
+        //Given
+        const alias = {
+            things: 'stuff'
+        }
+
+        //When
+        const isOAuth = creds.isOAuthAlias(alias);
+
+        //Then
+        expect(isOAuth).toBeFalsy();
+    });
+});
+
+describe('isPasswordAlias()', () => {
+
+    test('PasswordAlias is a PasswordAlias', () => {
+        //Given
+        const alias = factory.createPasswordAlias(new Date('2000-01-01'));
+
+        //When
+        const isPW = creds.isPasswordAlias(alias);
+
+        //Then
+        expect(isPW).toBeTruthy();
+    });
+
+    test('OAuthAlias is not a PasswordAlias', () => {
+        //Given
+        const alias = factory.createOAuthAlias(new Date('2000-01-01'));
+
+        //When
+        const isPW = creds.isPasswordAlias(alias);
+
+        //Then
+        expect(isPW).toBeFalsy();
+    });
+
+
+    test('Different object is not a PasswordAlias', () => {
+        //Given
+        const alias = {
+            things: 'stuff'
+        }
+
+        //When
+        const isPW = creds.isPasswordAlias(alias);
+
+        //Then
+        expect(isPW).toBeFalsy();
+    });
 });
 
 test('saveAlias() saves alias', () => {
