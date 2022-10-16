e39eeb23be5fb90daea4874a79a5352261a6458f
Dean Petersen <61034935+dwpetersen@users.noreply.github.com>
2022-10-16 11:28:29 +1100

    WIP - Added support for N dates.

--- src/query/types.ts
+++ src/query/types.ts
@@ -11,7 +11,21 @@ export function isDateLiteral(value: unknown) {
                              'LAST_QUARTER','NEXT_QUARTER','THIS_YEAR',
                              'NEXT_YEAR','THIS_FISCAL_QUARTER','LAST_FISCAL_QUARTER',
                              'NEXT_FISCAL_QUARTER','THIS_FISCAL_YEAR','NEXT_FISCAL_YEAR']);
-    return options.has(value as string);
+    if(!options.has(value as string)) {
+        return true;
+    }
+    else {
+        const nLiterals = new Set(['LAST_N_DAYS', 'NEXT_N_DAYS', 'N_DAYS_AGO',
+                                   'NEXT_N_WEEKS', 'LAST_N_WEEKS', 'N_WEEKS_AGO',
+                                   'NEXT_N_MONTHS', 'LAST_N_MONTHS', 'N_MONTHS_AGO', 
+                                   'NEXT_N_QUARTERS', 'LAST_N_QUARTERS', 'N_QUARTERS_AGO',
+                                   'NEXT_N_YEARS', 'LAST_N_YEARS', 'N_YEARS_AGO',
+                                   'NEXT_N_FISCAL_QUARTERS', 'LAST_N_FISCAL_QUARTERS', 'N_FISCAL_QUARTERS_AGO']);
+        
+        const [nLiteral, n] = (value as string).split(':');
+
+        return nLiterals.has(nLiteral) && Number.isInteger(Number.parseFloat(n));
+    }
 }
 
 export type LogicalOperator = 'AND'|'OR'|'NOT';
