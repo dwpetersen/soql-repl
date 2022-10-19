9b59b65ad7155d6a9c2281d248669b44a5c82c93
Dean Petersen <61034935+dwpetersen@users.noreply.github.com>
2022-10-20 09:08:57 +1100

    Fixed alias issue with examples/auth.js

--- examples/auth.js
+++ examples/auth.js
@@ -1,7 +1,8 @@
-const JS_PATH = '../../dist/';
-const creds = require(JS_PATH+'/auth/creds.js');
-const settings = require(JS_PATH+'/settings.js');
+const JS_PATH = '../dist/';
+const creds = require(JS_PATH+'auth/creds.js');
+const settings = require(JS_PATH+'settings.js');
 
-export alias = creds.openAlias(settings.defaultAlias);
+const alias = creds.openAlias(settings.settings.defaultAlias);
 
+module.exports = {alias}
 
