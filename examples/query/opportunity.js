7b1f074519aefa5fb68cfa50d1f3e24da98ed2a4
Dean Petersen <61034935+dwpetersen@users.noreply.github.com>
2022-10-19 22:53:50 +1100

    Created example for opportunity  query

---
+++ examples/query/opportunity.js
@@ -0,0 +1,41 @@
+const JS_PATH = '../../dist/'
+
+const query = require(JS_PATH+'./query/query.js');
+const alias = require('./alias.js');
+
+const runQuery = async () => {
+    let oppQuery = new query.SOQLQuery();
+    oppQuery.select('Id','Name','Amount')
+            .from('Opportunity')
+            .where('StageName')
+            .in('Closed Won', 'Prospecting')
+            .limit(5)
+            .build();
+    await oppQuery.execute(alias.alias);
+    return JSON.stringify(oppQuery.result, null, 2);
+}
+
+const run = async () => {
+    return await runQuery();
+}
+
+
+const handleThen = (data) => {
+    console.log(data);
+}
+
+const handleCatch = (err) => {
+    if (err.response) {
+        console.error(err.response.data);
+    }
+    else {
+        console.error(err);
+    }  
+}
+
+run().then(handleThen).catch(handleCatch);
+
+module.exports= {
+    run,
+    runQuery
+}
