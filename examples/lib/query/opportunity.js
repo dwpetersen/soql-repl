f904fe649acc19d80ca7a53b3ce25d5e54fe80b0
Dean Petersen <61034935+dwpetersen@users.noreply.github.com>
2022-10-21 22:34:34 +1100

    Exposed query and alias in opportunity.js lib example

--- examples/lib/query/opportunity.js
+++ examples/lib/query/opportunity.js
@@ -3,18 +3,28 @@ const JS_PATH = '../../../dist'
 const query = require(`${JS_PATH}/query/query.js`);
 const auth = require('../auth.js');
 
-const runQuery = async () => {
+const alias = auth.alias;
+
+const getQuery = () => {
     let oppQuery = new query.SOQLQuery();
     oppQuery.select('Id','Name','Amount')
-            .from('Opportunity')
-            .where('StageName')
-            .in('Closed Won', 'Prospecting')
-            .limit(5)
-            .build();
+        .from('Opportunity')
+        .where('StageName')
+        .in('Closed Won', 'Prospecting')
+        .limit(5)
+        .build();
+    return oppQuery;
+}
+
+const runQuery = async () => {
+    let oppQuery = getQuery();
     await oppQuery.execute(auth.alias);
     return JSON.stringify(oppQuery.result, null, 2);
 }
 
-module.exports= {
+module.exports = {
+    alias,
+    getQuery,
     runQuery
 }
+
